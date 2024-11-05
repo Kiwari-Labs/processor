e314935fd16248a8622ef2bfb12869ab18478cce
MASDXI <sirawitt42@gmail.com>
2024-11-05 17:23:53 +0700

    refactor: change mod to and ops
    Signed-off-by: MASDXI <sirawitt42@gmail.com>

--- src/datastructure/PU128LL.sol
+++ src/datastructure/PU128LL.sol
@@ -109,7 +109,7 @@ library PU128LL {
             l.n[e] = 0;
             l.s = s - 1;
             if (s > 1) {
-                if (s % 2 == 0) {
+                if (s & 2 == 0) {
                     (, mid) = get(l, mid); // move midpoint backward
                 } else {
                     (mid, ) = get(l, mid); // move midpoint forward
@@ -137,8 +137,8 @@ library PU128LL {
                     set(l, e, f, sn); // set element node{next:front, prev:sentinel}
                     set(l, sn, e, b); // set sentinel node{next:element, prev:back}
                     set(l, f, af, e); // set old front node{next:after front, prev:element}
-                    if (s > 1) {
-                        (, mid) = get(l, mid); // move midpoint backward
+                    if (s & 2 == 0) {
+                        (, mid) = get(l, mid); // Move midpoint backward
                     }
                 } else if (e > b) {
                     // push_back
@@ -146,7 +146,7 @@ library PU128LL {
                     set(l, e, sn, b); // set element node{next:sentinel, prev:back}
                     set(l, sn, f, e); // set sentinel node{next:front, prev:element}
                     set(l, b, e, bf); // set old back node{next:element prev:before back}
-                    if (s > 1) {
+                    if (s & 2 == 1) {
                         (mid, ) = get(l, l.md); // move midpoint forward
                     }
                 } else {
